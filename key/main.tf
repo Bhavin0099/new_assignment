d5b8f93cc9052d41f55df6965edf7054899a71d1
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-03-02 15:17:41 +0530

    Update main.tf

--- key/main.tf
+++ key/main.tf
@@ -8,13 +8,18 @@ resource "tls_private_key" "rsa-4096" {
 resource "aws_key_pair" "public_key" {
   key_name   = var.public_key_pair
   public_key = tls_private_key.rsa-4096.public_key_openssh
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.rsa-4096.private_key_pem}' > ./${public_key_pair}.pem"
+  }
 }
 
 resource "local_file" "private_key" {
   content = tls_private_key.rsa-4096.private_key_pem
   filename = var.private_key_pair
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.rsa-4096.private_key_pem}' > ./${private_key_pair}.pem"
+  }
 }
-
 
 
 #resource "aws_key_pair" "public_key_pair" {
