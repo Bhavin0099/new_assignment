24f0bd98075736638cab736d555d4f4a5b7bf6d2
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-03-03 02:39:36 +0530

    Update main.tf

--- terraform/modules/key/main.tf
+++ terraform/modules/key/main.tf
@@ -22,18 +22,4 @@ resource "local_file" "private_key" {
 }
 
 
-#resource "aws_key_pair" "public_key_pair" {
-#  key_name   = var.public_key_pair
-#  public_key = tls_private_key.private_key.public_key_openssh
-#} 
-#
-#
-#resource "tls_private_key" "private_key" {
-#  algorithm = "RSA"
-#  rsa_bits  = 4096
-#}
-#
-#resource "local_file" "private_key_file" {
-#  content  = tls_private_key.private_key.private_key_pem
-#  filename = var.private_key_name
-#}
+
