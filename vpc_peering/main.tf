6d06bebb9fbdc522ed2055e6524d9095247341a7
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-03-02 17:40:44 +0530

    Update main.tf

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -4,19 +4,30 @@ resource "aws_vpc_peering_connection" "vpc_peering_jenkins" {
   peer_vpc_id   = var.peer_vpc_id_created
   peer_region = var.peer_region
   vpc_id        = var.vpc_id_jenkins
-accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer_accepter" {
   provider = aws.peer
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering_jenkins.id
   auto_accept = true
+}
+
+resource "aws_vpc_peering_connection_options" "jenkins" {
+  provider                  = aws.jenkins
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer_accepter.id
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+resource "aws_vpc_peering_connection_options" "accepter" {
+  provider                  = aws.peer
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer_accepter.id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 }
 
 resource "aws_route" "peer_route" {
