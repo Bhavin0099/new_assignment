0fbcb136048fac7adcbfbea8e98ac30c0c790290
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-03-02 16:09:19 +0530

    Update main.tf

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -20,7 +20,7 @@ data "aws_vpc" "local_vpc" {
 
 resource "aws_route" "peer_route" {
   count           = length(var.route_table_ids)
-  route_table_id  = var.route_table_ids[count.index]
+  route_table_id  = var.route_table_ids_from_vpc_peering[count.index]
   destination_cidr_block = data.aws_vpc.local_vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering_jenkins.id
 }
