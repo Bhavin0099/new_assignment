8814af879d79399907a6306debf08585610c601a
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-03-02 17:21:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ module "for_vpc" {
 
 module "for_vpc_peering" {
   providers = {
-    aws = aws.usw2
+    aws = aws.jenkins
+    aws = aws.peer
   }
   source = "./vpc_peering"
   peer_vpc_id_created = module.for_vpc.vpc_output
