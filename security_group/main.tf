96dbb1efbf26569ad36260d146fa99c7b8a4380d
Bhavin0099 <153531232+Bhavin0099@users.noreply.github.com>
2024-03-02 18:21:01 +0530

    Update main.tf

--- security_group/main.tf
+++ security_group/main.tf
@@ -44,7 +44,22 @@ resource "aws_security_group" "ES_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 1024
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = var.SG_tag
   }
